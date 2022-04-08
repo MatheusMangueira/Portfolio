8d83a31917a07e12b8312bbe792cd63ad8d2cb2a
Matheus Mangueira <98111351+MatheusMangueira@users.noreply.github.com>
2022-04-08 20:44:05 -0300

    at

--- index.js
+++ index.js
@@ -2,6 +2,30 @@ var btn_html = document.getElementById("html");
 var btn_css = document.getElementById("css");
 var btn_js = document.getElementById("js");
 var btn_reactjs = document.getElementById("reactjs");
+var menu_itens = document.querySelectorAll(".menu a[href^='#']");
+
+
+menu_itens.forEach(item => {
+    item.addEventListener("click", scrollitens);
+})
+
+function getScrollTopByHref(element){
+    const id = element.getAttribute("href");
+    return document.querySelector(id).offsetTop;
+}
+
+function scrollitens(event){
+    event.preventDefault();
+    const to = getScrollTopByHref(event.target) - 65.59; 
+    scrollToPosition(to);
+}
+
+function scrollToPosition(to){
+    window.scroll({
+        top: to,
+        behavior: "smooth"
+    });
+}
 
 
 btn_html.addEventListener("click", function() {
@@ -27,28 +51,3 @@ btn_reactjs.addEventListener("click", function () {
     document.getElementById("texto").innerText = "React is the most popular JavaScript library and is used to build a user interface (UI). It provides an excellent response for the user to add commands using a new method of rendering websites. The components of this tool were developed by Facebook."
 
 })
-
-function quem_sou(){
-
-    
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-// function clicar(){
-
-//     let test = document.getElementById("texto").innerText = "Linguagem de estilização"
-   
-
-
-// }
